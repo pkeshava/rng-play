1b37207261a034e5560196bd58d175c61050af4f
Pouyan <pouyank13@me.com>
2017-04-01 13:18:40 -0600

    creating struct and printing values from it

--- helloemacs.c
+++ helloemacs.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
+#include <math.h>
+
+typedef struct modulation {
+   char* QPSK;
+   int q;
+} mod;
 
 int main(){
-  printf("Hello Emacs World!\n");
-  int x[]= {1,2,3,4,5};
-  int y = x[2];
-  printf("y = %d \n", y);
-  int z = 5;
-  int dild = 2;
+
+
+printf("Hello Emacs World!\n");
+int x[]= {1,2,3,4,5};
+int y = x[2];
+printf("y = %d \n", y);
+int z = 5;
+int dild = 2;
+mod mod1 = {"QPSK",256};
+printf("The modulation type is %s, %d \n", mod1.QPSK, mod1.q);
+
 }
