d37cec35b92eceb386d568c6a0f54d4bbfd51e3e
Pouyan <pouyank13@me.com>
2017-04-03 17:37:28 -0600

    Created typedefs file and did small printf test in main

--- cplusdev.cpp
+++ cplusdev.cpp
@@ -2,22 +2,31 @@
 
 #include <stdio.h>
 #include <math.h>
+#include "typedefinitions.h"
 
-using namespace std;
+//using namespace std;
+
+int main(){
+
+u32 boyz = 5;
+printf("U32 test is %lu \n", boyz);
+
+}
+
+/*------ sample code -----------------
 typedef struct modulation {
    char* QPSK;
    int q;
 } mod;
 
-int main(){
-
-//printf("Hello Emacs World!\n");
 int x[]= {1,2,3,4,5};
+printf("Hello Emacs World!\n");
 int y = x[2];
 printf("y = %d \n", y);
 int z = 5;
 int dild = 2;
 mod mod1 = {"QPSK",256};
 printf("The modulation type is %s, %d \n", mod1.QPSK, mod1.q);
+----------
 
-}
+*/
---
+++ typedefinitions.h
@@ -0,0 +1,39 @@
+#ifndef typedefinitions_h
+#define typedefinitions_h
+
+typedef char CHAR;                   // Signed 8 bits
+typedef unsigned char UCHAR;         // Unsigned 8 bits
+typedef short SHORT;                 // Signed 16 bits
+typedef unsigned short USHORT;       // Unsigned 16 bits
+typedef long LONG;                   // Signed 32 bit
+typedef unsigned long ULONG;         // unsigned 32 bit
+typedef bool BOOL;                   // BOOLEAN VALUE!!!!
+
+typedef long s32;
+typedef short s16;
+typedef signed char  s8;
+
+typedef const long sc32;            // Read Only
+typedef const short sc16;           // Read Only
+typedef const char sc8;             // Read Only
+
+typedef ULONG  u32;                 // Read Only
+typedef USHORT u16;                 // Read Only
+typedef UCHAR  u8;                  // Read Only
+
+typedef const ULONG uc32;           // Read Only
+typedef const USHORT uc16;          // Read Only
+typedef const USHORT uc8;           // Read Only
+
+// Not sure why this code is here... bool is already a tpye in C++... INVESTIGATE
+// Perhaps just to show that
+#ifndef __cplusplus
+typedef enum {FALSE = 0, TRUE = !FALSE} bool;
+#endif
+// These are not strictly necessary but might be useful
+typedef enum {RESET = 0, SET = !RESET} FlagStatus, ITStatus;
+typedef enum {DISABLE = 0, ENABLE = !DISABLE} FunctionalState;
+typedef enum {ERROR = 0, SUCCESS = !ERROR} ErrorStatus;
+
+
+#endif                              // typedefinitions.h
