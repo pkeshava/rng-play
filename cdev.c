f73201f7aebd4516174a7bb7a21506711d6b9725
Pouyan <pouyank13@me.com>
2019-03-05 11:35:58 +0100

    I think there is an issue with size t. Going to change everything to int

--- cdev.c
+++ cdev.c
@@ -11,48 +11,42 @@ This code is used as a first step dive into the world of true random number gene
 
 int main(int argc, char* argv[]){
 
-  size_t nBytes = 10;
+  size_t nBytes;
   int counter;
-  printf("Program Name Is: %s",argv[0]);
-  if(argc == 1)
+  printf("Program Name Is: %s\n",argv[0]);
+  if(argc == 1){
     printf("\n\nNo extra command line arguments passed other than the program name.\nTherefore, there are 10 total random bytes generated and the probability of 1's and 0's is equal. \nConversely, you can generate the number of bytes required and the probability bias for number of 1's with command line arguments. \nFor example, type in your command prompt:\n'./cdev -N 512 -p 0.35'");
+    nBytes = 10;
+  }
   else if(argc > 5)
   {
     //check to see if too many arguments given
     printf("\nFailed: Too many arguments passed.\nPlease specify total number of Bytes generated with option -N and the probability bias for the number of 1's (between 0 and 0.50) with option -p\ni.e type in your command prompt:\n'./cdev -N 512 -p 0.35'\n");
     exit(1);
   }
-  else if (5 > argc >= 1)
+  else if (argc > 1 && argc < 6)
   {
     //test for now
-    printf("\n\nNumber Of Arguments Passed: %d",argc);
-    printf("\n\n----Following Are The Command Line Arguments Passed----");
-    for(counter=0;counter<argc;counter++)
-      printf("\n\nargv[%d]: %s",counter,argv[counter]);
+    int i;
+    for (i = 1; i < argc; i++)  /* Skip argv[0] (program name). */
+    {
+      if (strcmp(argv[i], "-N") == 0){
+        char *p;
+        long conv = strtol(argv[i+1], &p, 10);
+        if (errno != 0 || *p != '\0' || conv > INT_MAX) {
+          printf("Failed");
+          exit(1);
+        }
+        nBytes = conv;
+        printf("Number of random bytes generated is %zu\n", nBytes);
+      }
+      else
+        nBytes = 8;
+    }
   }
-  // int i;
-  // int quiet = 0;  /* Value for the "-q" optional argument. */
-  // for (i = 1; i < argc; i++)  /* Skip argv[0] (program name). */
-  // {
-  //   if (strcmp(argv[i], "-N") == 0)  /* Process optional arguments. */
-  //   {
-  //     nBytes = 8;  /* This is used as a boolean value. */
-  //   }
-  //   else
-  //   {
-  //     char *p;
-  //     long conv = strtol(argv[i+1], &p, 10);
-  //     if (errno != 0 || *p != '\0' || conv > INT_MAX) {
-  //       perror("Failed: ");
-  //     }
-  //     else {
-  //         // No error
-  //         nBytes = conv;
-  //         printf("Number of random bytes generated is %zu\n", nBytes);
-  //     }
-  //     /* Process non-optional arguments here. */
-  //   }
-  // }
+  else {
+    nBytes = 11;  /* if no value is given then */
+  }
 
   srand((unsigned int) time (NULL));
   u8* bstream;
