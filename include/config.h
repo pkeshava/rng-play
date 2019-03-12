551b157cd92d46c13cbfa5b4fa8ec8ae1b9b57f0
Pouyan <pouyank13@me.com>
2019-03-12 10:16:28 +0100

    looking to add bias by implementing the uniform distribution from [0,1) as Mario suggested. Note, that using the method currently used will likely have low entropy for lower bits. I've added a link in the README that explains this

--- include/config.h
+++ include/config.h
@@ -10,6 +10,8 @@
 #include <string.h>
 #include <math.h>
 #include <time.h>
+#include "float.h"
+
 
 
 #endif
--- src/cdev.c
+++ src/cdev.c
@@ -6,26 +6,28 @@ This code is used as a first step dive into the world of true random number gene
 
 #include "config.h"
 
+
 int main(int argc, char** argv){
 
   int nBytes;
   float prob1s;
 
-   stateVariables stateVars = {.nBytes = 0, .argc = argc, .prob1s = 0.5, .argv = argv};
+  stateVariables stateVars = {.nBytes = 0, .argc = argc, .prob1s = 0.5, .argv = argv};
 
   stateVars = determineSequence(stateVars);
   nBytes = stateVars.nBytes;
   prob1s = stateVars.prob1s;
 
-  srand((unsigned int) time (NULL));
+
+  srand((unsigned int) time (NULL)); // create a seed by reading epoch time
   u8* bstream;
   printf("\n");
   // Generate random bits
-  bstream = gen_rdm_bytestream(nBytes);
+  bstream = gen_rdm_bytestream(nBytes); // also prints bytes as ascii characters
   printf("\n");
-  //printf ("Random string: %s\n",bstream);
+
   for(int i = 0; i<nBytes; i++)
-    printf("%u ", bstream[i]); // prints a series of bytes as ascii characters
+    printf("%u ", bstream[i]); // prints a series of bytes as the decimal representation of the ascii byte i.e 122 104 120 etc..
   printf("\n\n");
   // write to a binary file and then read back to check it was done correctly
   writeToFile(bstream, nBytes);
@@ -33,23 +35,31 @@ int main(int argc, char** argv){
   buffer = readFromFile(nBytes);
 
   for(int i = 0; i<nBytes; i++)
-    printf("%u ", buffer[i]); // prints a series of bytes.. this now should print exactly the same as ascii numbers only properly split up
-
+    printf("%u ", buffer[i]); // This now should print exactly the same decimal representations of ascii bytes as done before.
   printf("\n\n");
+
+  // Now convert the bytes to bit representation and print on the command line
   printBits(nBytes, buffer); // NOTE if this is done without the dynamic memory allocation then you need to have &buffer
   printf("\n");
 
   free(bstream);
   free(buffer);
 
+  printf("test: %d\n\n", RAND_MAX);
+  double testRnd = rand()%4;
+  double normalized = (double) rand() / nextafter(RAND_MAX, DBL_MAX);
+  printf("test: %f\n\n", testRnd);
+  printf("test: %f\n\n", normalized);
+
+
 return 0;
 }
 
 /* Tasks Remaining
     - Create boolean algebra table and algorithm for biasing the bitstream with a percentage of 1's
-    - Take command line input for number of bytes and the percentage of ones... do calculations to determine if possible and output error message if required
+    - Take command line input for number of bytes and the percentage of ones... do calculations to determine if possible and output error message if required DONE
     - python analysis of data file to calculate autocorrelation, cross correlation shannon entropy etc.
     - calculate total runtime of the program
-    - adjust makefile
+    - adjust makefile DONE
     - C/python library for nist test or any other statistical testing
 */
